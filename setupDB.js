4f7f9e69f3c0d8e018656e4513ab63a57f31ff59
FranciscoSolerAguado <franciscojsoleraguado@gmail.com>
2026-05-07 11:20:35 +0200

    Actualizar BD.

--- setupDB.js
+++ setupDB.js
@@ -16,15 +16,15 @@ db.serialize(() => {
 
     // Insertamos la lista oficial
     stmt.run("Guardia", 50, 10, "Regimiento base del ejército enemigo. Equipados con armaduras ligeras y espadas cortas, patrulla las fronteras del reino.");
-    stmt.run("Ratcher", 25, 7, "Meticulosos y escurridizos, esta compañia de roedores disparará a distancia con una precisión inhumana");
+    stmt.run("Ratcher", 25, 7, "Meticulosos y escurridizos, esta compañia de roedores disparará a distancia con una precisión increible.");
     stmt.run("Elite", 250, 20, "División de veteranos de incontables escaramuzas fronterizas. Portan una armaduras pesadas forjadas en las profundidades.");
     stmt.run("Boss", 1000, 35, "Líderes despiadados que han unificado a las tribus salvajes bajo un solo estandarte de guerra, la mayoría de sus integrantes no han conocido nunca la derrota en combate.");
     stmt.run("Solid Snake", 100, 13, "Infantería media y tenaz de las fuerzas pantanosas, expertas en terrenos boscosos y pantanos. Avanza sin piedad tras su robusto escudo.");
     stmt.run("Maestro de las Pociones", 30, 13, "Salidos de la universidad del reino enemigo, estos excéntricos maestros que lanzan brebajes tóxicos y líquidos explosivos fabricados en su laboratorio al enemigo.");
     stmt.run("Comandante", 500, 25, "Estrategas y guerreros brillantes que coordinan los ataques tácticos desde la retaguardia del campo de batalla, cuando la situación se torna complicada en el campo de batalla, estos se lanzan a la batalla.");
-    stmt.run("Guardian del Pantano", 2000, 50, "Se habla mucho de estos colosos escamosos y nada bueno, entre tantas, nunca nadie ha conseguido a alguno de ellos, se dice que no existen mas de 15.");
-    stmt.run("Nutria Gigante", 150, 20, "Pescadores que han adaptado su arpón dentado para lanzarlo a distancia media al enemigo.");
-    stmt.run("Tirachinas", 40, 4, "Pequeños y equipados con un tirachinas, se camuflaran con el entorno y se subiran en arboles para lanzar su ataques a distancia. ");
+    stmt.run("Guardian del Pantano", 2000, 50, "Se habla mucho de estos colosos escamosos y nada bueno, entre tantas, nunca nadie ha conseguido matar a alguno de ellos, se dice que no existen mas de 15.");
+    stmt.run("Nutria Gigante", 150, 20, "Pescadores muy duros que han adaptado su arpón dentado para lanzarlo a distancia media al enemigo.");
+    stmt.run("Tirachinas", 40, 4, "Pequeños y equipados con un tirachinas, la distancia a la que pueden atacar es inhumana y son muy molestos, les gusta matar Mineros del Reino.");
     stmt.run("Reina Tortuga", 8000, 100, "Monarca absoluto de las profundidades. Su caparazón es más resistente que el acero templado. Rey del reino enemigo y valiente guerrero, el ultimo enemigo que debe ser derrotado.");
 
     stmt.finalize();
